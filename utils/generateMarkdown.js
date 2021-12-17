1aba65fab0d55417ecc6614306dfcc750defd71c
Mark Dale <msdaledad@gmail.com>
2021-12-16 21:39:46 -0500

    filling out readme (iter26)

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,82 @@
 const fse = require('fs-extra');
 
+const renderLicenseBadge = async (promiseParams) => {
+
+  switch (promiseParams.readmeParams.license.toLowerCase()) {
+    case "mit":
+      // copy the image to dist/assets/images dir...
+      copyFile("./assets/images/license-MIT-green.svg", promiseParams.imagesDir + "/license-MIT-green.svg")
+        .then(targetFilePath => {
+          console.log("Copied ./assets/images/license-MIT-green.svg to " + targetFilePath);
+          // transform MIT pre-text to dated version
+          return readFile("./assets/license-docs/pretext/MIT-pre.txt");
+        })
+        .then(data => {
+          console.log("Read file ./assets/license-docs/pretext/MIT-pre.txt");
+          const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
+          return writeFile(promiseParams.preTextDir + "/" + "MIT-pre.txt", datedNamedData);
+        })
+        .then(targetFilePath => {
+          console.log("Created file " + targetFilePath);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+      return '[![License: MIT](./assets/images/license-MIT-green.svg)](./assets/license-docs/pretext/MIT-pre.txt)';
+
+    case "gpl v3":
+      // transform GPL-v3 pre-text to dated version
+      readFile("./assets/license-docs/pretext/gpl-v3-pre.txt")
+        .then(data => {
+          console.log("Read file ./assets/license-docs/pretext/gpl-v3-pre.txt");
+          const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
+          console.log(datedNamedData);
+          return writeFile(promiseParams.preTextDir + "/" + "gpl-v3-pre.txt", datedNamedData);
+        })
+        .then(targetFilePath => {
+          console.log("Created file " + targetFilePath);
+          // copy the image to dist/assets/images dir...
+          return copyFile("./assets/images/license-GPLv3-blue.svg", promiseParams.imagesDir + "/license-GPLv3-blue.svg");
+        })
+        .then(targetFilePath => {
+          console.log("Copied ./assets/images/license-GPLv3-blue.svg " + targetFilePath);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+      return '[![License: GPL v3](./assets/images/license-GPLv3-blue.svg)](./assets/license-docs/pretext/gpl-v3-pre.txt)';
+
+    case "gpl v2":
+      // copy the image to dist/assets/images dir...
+      copyFile("./assets/images/license-GPL_v2-blue.svg", promiseParams.imagesDir + "/license-GPL_v2-blue.svg")
+        .then(targetFilePath => {
+          console.log("Copied ./assets/images/license-GPL_v2-blue.svg " + targetFilePath);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+      return '[![License: GPL v2](./assets/images/license-GPL_v2-blue.svg)](./assets/license-docs/pretext/gpl-v2-pre.txt)';
+
+    case "lgpl v3":
+      // copy the image to dist/assets/images dir...
+      copyFile("./assets/images/license-LGPL_v3-blue.svg", promiseParams.imagesDir + "/license-LGPL_v3-blue.svg")
+        .then(targetFilePath => {
+          console.log("Copied ./assets/images/license-LGPL_v3-blue.svg " + targetFilePath);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+
+      return '[![License: LGPL v3](./assets/images/license-LGPL_v3-blue.svg)](./assets/license-docs/pretext/lgpl-v3-pre.txt)';
+
+    default:
+      return '';
+  }
+};
+
 const renderTitle = async (promiseParams) => {
   return `# ${promiseParams.readmeParams.title}
 
@@ -80,23 +157,8 @@ const renderLicense = async (promiseParams) => {
 
   switch (promiseParams.readmeParams.license.toLowerCase()) {
     case "mit":
-      // copy the image to dist/assets/images dir...
-      copyFile("./assets/images/license-MIT-green.svg", promiseParams.imagesDir + "/license-MIT-green.svg")
-        .then(targetFilePath => {
-          console.log("Copied ./assets/images/license-MIT-green.svg to " + targetFilePath);
-          // transform MIT pre-text to dated version
-          return readFile("./assets/license-docs/pretext/MIT-pre.txt");
-        })
-        .then(data => {
-          console.log("Read file ./assets/license-docs/pretext/MIT-pre.txt");
-          const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
-          return writeFile(promiseParams.preTextDir + "/" + "MIT-pre.txt", datedNamedData);
-        })
-        .then(targetFilePath => {
-          console.log("Created file " + targetFilePath);
-          // transform MIT full disclosure text to dated version
-          return readFile("./assets/license-docs/full-disclosure/MIT.txt");
-        })
+      // transform MIT full disclosure text to dated version
+      readFile("./assets/license-docs/full-disclosure/MIT.txt")
         .then(data => {
           console.log("Read file ./assets/license-docs/full-disclosure/MIT.txt");
           const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
@@ -111,32 +173,13 @@ const renderLicense = async (promiseParams) => {
 
       return `## License
 
-[![License: MIT](./assets/images/license-MIT-green.svg)](./assets/license-docs/pretext/MIT-pre.txt)
-
 [Full Disclosure](./assets/license-docs/full-disclosure/MIT.txt)
 
 `;
 
-
     case "gpl v3":
-      // copy the image to dist/assets/images dir...
-      copyFile("./assets/images/license-GPLv3-blue.svg", promiseParams.imagesDir + "/license-GPLv3-blue.svg")
-        .then(targetFilePath => {
-          console.log("Copied ./assets/images/license-GPLv3-blue.svg " + targetFilePath);
-          // transform GPL-v3 pre-text to dated version
-          return readFile("./assets/license-docs/pretext/gpl-v3-pre.txt");
-        })
-        .then(data => {
-          console.log("Read file ./assets/license-docs/pretext/gpl-v3-pre.txt");
-          const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
-          console.log(datedNamedData);
-          return writeFile(promiseParams.preTextDir + "/" + "gpl-v3-pre.txt", datedNamedData);
-        })
-        .then(targetFilePath => {
-          console.log("Created file " + targetFilePath);
-          // transform GPL-v3 full disclosure text to dated version
-          return readFile("./assets/license-docs/full-disclosure/gpl-v3.txt");
-        })
+      // transform GPL-v3 full disclosure text to dated version
+      readFile("./assets/license-docs/full-disclosure/gpl-v3.txt")
         .then(data => {
           console.log("Read file ./assets/license-docs/full-disclosure/gpl-v3.txt");
           const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
@@ -151,63 +194,47 @@ const renderLicense = async (promiseParams) => {
 
       return `## License
 
-[![License: GPL v3](./assets/images/license-GPLv3-blue.svg)](./assets/license-docs/pretext/gpl-v3-pre.txt)
-
 [Full Disclosure](./assets/license-docs/full-disclosure/gpl-v3.txt)
 
 `;
 
     case "gpl v2":
-      // copy the image to dist/assets/images dir...
-      copyFile("./assets/images/license-GPL_v2-blue.svg", promiseParams.imagesDir + "/license-GPL_v2-blue.svg")
-        .then(targetFilePath => {
-          console.log("Copied ./assets/images/license-GPL_v2-blue.svg " + targetFilePath);
-          // copy the pretext license file to dist/assets/license-docs/pretext dir...(substitute data if necessary)
-          return copyFile("./assets/license-docs/pretext/gpl-v2-pre.txt", promiseParams.preTextDir + "/gpl-v2-pre.txt");
+      // transform GPL-v2 full disclosure text to dated version
+      readFile("./assets/license-docs/full-disclosure/gpl-v2.txt")
+        .then(data => {
+          console.log("Read file ./assets/license-docs/full-disclosure/gpl-v2.txt");
+          const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
+          return writeFile(promiseParams.fullDisclosureDir + "/" + "gpl-v2.txt", datedNamedData);
         })
         .then(targetFilePath => {
-          console.log("Copied  ./assets/license-docs/pretext/gpl-v2-pre.txt to " + targetFilePath);
-          // copy the full-disclosure license file to the dist/assets/license-docs/full-disclosure dir...(substitute data if necessary)
-          return copyFile("./assets/license-docs/full-disclosure/gpl-v2.txt", promiseParams.fullDisclosureDir + "/gpl-v2.txt");
-        })
-        .then(targetFilePath => {
-          console.log("Copied  ./assets/license-docs/full-disclosure/gpl-v2.txt to " + targetFilePath);
+          console.log("Created file " + targetFilePath);
         })
         .catch(err => {
           console.log(err);
         });
 
       return `## License
-
-[![License: GPL v2](./assets/images/license-GPL_v2-blue.svg)](./assets/license-docs/pretext/gpl-v2-pre.txt)
 
 [Full Disclosure](./assets/license-docs/full-disclosure/gpl-v2.txt)
 
 `;
 
     case "lgpl v3":
-      // copy the image to dist/assets/images dir...
-      copyFile("./assets/images/license-LGPL_v3-blue.svg", promiseParams.imagesDir + "/license-LGPL_v3-blue.svg")
-        .then(targetFilePath => {
-          console.log("Copied ./assets/images/license-LGPL_v3-blue.svg " + targetFilePath);
-          // copy the pretext license file to dist/assets/license-docs/pretext dir...(substitute data if necessary)
-          return copyFile("./assets/license-docs/pretext/lgpl-v3-pre.txt", promiseParams.preTextDir + "/lgpl-v3-pre.txt");
+      // transform LGPL-v3 full disclosure text to dated version
+      readFile("./assets/license-docs/full-disclosure/lgpl-v3.txt")
+        .then(data => {
+          console.log("Read file ./assets/license-docs/full-disclosure/lgpl-v3.txt");
+          const datedNamedData = data.toString().replace("<year>", year).replace("<fullname>", name);
+          return writeFile(promiseParams.fullDisclosureDir + "/" + "lgpl-v3.txt", datedNamedData);
         })
         .then(targetFilePath => {
-          console.log("Copied  ./assets/license-docs/pretext/lgpl-v3-pre.txt to " + targetFilePath);
-          // copy the full-disclosure license file to the dist/assets/license-docs/full-disclosure dir...(substitute data if necessary)
-          return copyFile("./assets/license-docs/full-disclosure/lgpl-v3.txt", promiseParams.fullDisclosureDir + "/lgpl-v3.txt");
-        })
-        .then(targetFilePath => {
-          console.log("Copied  ./assets/license-docs/full-disclosure/lgpl-v3.txt to " + targetFilePath);
+          console.log("Created file " + targetFilePath);
         })
         .catch(err => {
           console.log(err);
         });
 
       return `## License
-
-[![License: LGPL v3](./assets/images/license-LGPL_v3-blue.svg)](./assets/license-docs/pretext/lgpl-v3-pre.txt)
 
 [Full Disclosure](./assets/license-docs/full-disclosure/lgpl-v3.txt)
 
